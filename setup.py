02ea73e8c24b942104a1921733bce2c100bd4c92
Gilmar Pereira dos Santos <34770823+amgsnt@users.noreply.github.com>
2019-02-21 21:35:42 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from glob import glob
+
+from setuptools import find_packages
+
+setup(name='Hello',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Gilmar P Santos',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+     )
