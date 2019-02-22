bd3b2f5dfc7510d5f406e15cb3ed27b032ba3c41
Gilmar Pereira dos Santos <34770823+amgsnt@users.noreply.github.com>
2019-02-22 15:20:26 -0300

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,7 +5,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 
 from chat.hello import app
 
-with app.test_client() as c:
-    response = c.get('/')
-    assert response.data == b'Hello World!'
-    assert response.status_code == 200
+def test_empty():
+    with app.test_client() as c:
+        response = c.get('/')
+        assert response.data == b'Hello World!'
+        assert response.status_code == 200
