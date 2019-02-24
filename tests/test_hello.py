407bc9ef788101c928e7dda5919b72f3d1c4488f
Gilmar Pereira dos Santos <34770823+amgsnt@users.noreply.github.com>
2019-02-24 12:11:42 -0300

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -6,5 +6,5 @@ from chat.hello import app
 def test_empty():
     with app.test_client() as c:
         response = c.get('/')
-        assert response.data == b'<b>Bia linda da minha vida!</b>'
+        assert response.data == b'<b>Hello!</b>'
         assert response.status_code == 200
